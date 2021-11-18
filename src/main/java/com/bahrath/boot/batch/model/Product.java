3c1ad50554a97259e512131386b2caa6fc04cfb6
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-18 17:39:20 -0500

    First commit

---
+++ src/main/java/com/bahrath/boot/batch/model/Product.java
@@ -0,0 +1,15 @@
+package com.bahrath.boot.batch.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+
+}
