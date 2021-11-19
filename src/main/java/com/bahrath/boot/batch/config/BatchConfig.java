ee941dc0314868f131f255e95b9fefd98ba5a9ff
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-19 11:01:54 -0500

    Impliment Writer

--- src/main/java/com/bahrath/boot/batch/config/BatchConfig.java
+++ src/main/java/com/bahrath/boot/batch/config/BatchConfig.java
@@ -3,6 +3,9 @@ package com.bahrath.boot.batch.config;
 import com.bahrath.boot.batch.model.Product;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -32,11 +35,19 @@ public class BatchConfig {
         return reader;
     }
 
+    @Bean
     public ItemProcessor<Product, Product> processor() {
         return (p) -> {
             p.setPrice(p.getPrice()-p.getPrice()*10/100);
             return p;
         };
+    }
+
+    public ItemWriter<Product> writer() {
+        JdbcBatchItemWriter<Product> writer = new JdbcBatchItemWriter<>();
+        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Product>());
+        writer.setSql("INSERT INTO PRODUCT (ID, NAME, DESCRIPTION, PRICE) VALUES (:id, :name, :description, :price)");
+        return writer;
     }
 
 }
