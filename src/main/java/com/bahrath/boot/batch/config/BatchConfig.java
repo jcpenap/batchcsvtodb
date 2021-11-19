b5a6da81c74d99a8cad7cc48a219ee89a93a413a
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-19 11:20:46 -0500

    Adding Datasource, Step and Job

--- src/main/java/com/bahrath/boot/batch/config/BatchConfig.java
+++ src/main/java/com/bahrath/boot/batch/config/BatchConfig.java
@@ -1,6 +1,11 @@
 package com.bahrath.boot.batch.config;
 
 import com.bahrath.boot.batch.model.Product;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
@@ -10,6 +15,7 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -19,6 +25,28 @@ import javax.sql.DataSource;
 
 @Configuration
 public class BatchConfig {
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    public Job job() {
+        return jobBuilderFactory.get("job-2")
+                .incrementer(new RunIdIncrementer())
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Step step() {
+        return stepBuilderFactory.get("step-1").<Product, Product>chunk(3)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
 
     @Bean
     public ItemReader<Product> reader() {
